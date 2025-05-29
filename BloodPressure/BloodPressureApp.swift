6731238ac96ca0e55f9efd5ad3865375ddd8c441
Gordon Aspin <gordon.aspin@gmail.com>
2025-05-29 10:23:15 -0400

    Initial Commit

---
+++ BloodPressure/BloodPressureApp.swift
@@ -0,0 +1,17 @@
+//
+//  BloodPressureApp.swift
+//  BloodPressure
+//
+//  Created by Gordon Aspin on 5/29/25.
+//
+
+import SwiftUI
+
+@main
+struct BloodPressureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BloodPressureTests/BloodPressureTests.swift
@@ -0,0 +1,17 @@
+//
+//  BloodPressureTests.swift
+//  BloodPressureTests
+//
+//  Created by Gordon Aspin on 5/29/25.
+//
+
+import Testing
+@testable import BloodPressure
+
+struct BloodPressureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
